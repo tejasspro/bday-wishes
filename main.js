9e93c4f660847fadbfd7578d72142d9dc0dba579
tejasspro <86661753+tejasspro@users.noreply.github.com>
2021-11-26 18:00:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	
+    fabric.Image.fromURL('Birthdayimage.jpg', function(Img) {
+
+        block_Image_obj = Img;
+        block_Image_obj.scaleToWidth(700);
+        block_Image_obj.scaleToHieght(510);
+        block_Image_obj.set({
+            top:0, left:0
+        });
+        canvas.add(block_Image_obj);
+        });
+
+}
+
+function playsound(){
+	x.play();
+}
+
+fabric.Image.fromURL(get_image, function(Img){
+    block_Image_obj.set({
+        top:0,
+        left:0
+    });
+    canvas.add(block_Image_obj);
+});
